06db4629f32fe1c67e496bbf331b2f6f6bc51643
Maksim Baulin <maksimbaulin92@gmail.com>
2026-08-24 21:42:04 +0700

    fix

--- src/api/use-register-mutation.ts
+++
@@ -1,9 +0,0 @@
-import { useMutation } from "@tanstack/react-query";
-import { post } from "./api";
-
-export const useRegisterMutation = () => {
-  return useMutation({
-    mutationFn: (input: string) =>
-      post<string, string>("/api/auth/register", input),
-  });
-};
